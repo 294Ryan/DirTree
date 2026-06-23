291837a1ac18a6619bb6ce11b8156e2c77a6754b
Coder <294ryan@gmail.com>
2026-06-23 14:37:57 +0800

    Update main.py to v1

--- main.py
+++ main.py
@@ -1,64 +1,108 @@
 import sys
-import os
 import os.path
-import tkinter.messagebox as tkm
-from colorama import init, Fore, Back, Style
+import pyperclip as pc
+from pathlib import Path
+from colorama import init, Fore, Back
 
 class TextTheme:
     ERROR       = Back.RESET + Fore.LIGHTRED_EX
-    INFO        = Back.RESET + Fore.CYAN
     WARNING     = Back.RESET + Fore.YELLOW
-    APP_TITLE   = Back.RESET + Fore.BLUE
+    INFO        = Back.RESET + Fore.LIGHTBLACK_EX
+    APP_TITLE   = Back.RESET + Fore.CYAN
     PROMPT      = Back.RESET + Fore.LIGHTBLACK_EX
     INPUT       = Back.RESET + Fore.CYAN
-    OUTPUT      = Back.RESET + Fore.WHITE
-    USER_LABEL  = Back.LIGHTBLUE_EX + Fore.BLACK
     NONE        = Back.RESET + Fore.RESET
-    HELP        = Back.RESET + Fore.LIGHTBLACK_EX
 
 
-def resourcePath(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-def showInfo(msg, msgboxTitle = ""):
+def showInfo(msg):
     printMsg = f"\n[Info] {msg}"
     print(TextTheme.INFO + printMsg)
-    if msgboxTitle:
-        tkm.showinfo(msgboxTitle, printMsg)
 
-def showWarning(msg, msgboxTitle = ""):
+def showWarning(msg):
     printMsg = f"\n[Warning] {msg}"
     print(TextTheme.WARNING + printMsg)
-    if msgboxTitle:
-        tkm.showwarning(msgboxTitle, printMsg)
 
-def showError(msg, msgboxTitle = ""):
+def showError(msg):
     printMsg = f"\n[Error] {msg}"
     print(TextTheme.ERROR + printMsg)
-    if msgboxTitle:
-        tkm.showerror(msgboxTitle, printMsg)
 
 def firstPage():
     print(TextTheme.APP_TITLE + "<< Directory Walker >>")
     print(TextTheme.PROMPT + 
 """\n
--Product: Directory Walker
--Function: A tool used to scan a specified directory and output the results as a tree structure.
--Version: v1.0.0
--Developer: 294Ryan
+- Product: Directory Walker
+- Function: A tool used to scan a specified directory and output the results as a tree structure.
+- Version: v1.0.0
+- Developer: 294Ryan
 """)
 
+def getInput():
+    temp = input(TextTheme.INPUT + ">>")
+    return temp
+
+def p(arg):
+    print(TextTheme.NONE + arg, end = "")
+    return arg
+
+def newLine():
+    print(TextTheme.NONE + "")
+    return "\n"
+
+def printTree(deepen, oPath):
+    """ ├ │ └ ─ """
+    global fullTree
+    fullPath = oPath.resolve()
+    files = sorted([x for x in oPath.iterdir() if x.is_file()])
+    dirs = sorted([x for x in oPath.iterdir() if x.is_dir()])    
+    
+    fullTree += p(fullPath.name)
+    fullTree += p(r"/")
+    fullTree += newLine()
+    
+    items = dirs + files
+    total = len(items)
+    
+    for idx, item in enumerate(items):
+        fullTree += p("│  " * deepen if deepen > 0 else "")
+        fullTree += p("└─" if idx == total - 1 else "├─")
+        if item.is_dir():
+            printTree(deepen + 1, item)
+        else:
+            fullTree += p(item.name)
+            fullTree += newLine()
+    
 def main():
+    global fullTree
     init()
     firstPage()
-
-
-
-
+    
+    while True:
+        print(TextTheme.INPUT + "Enter a directory path")
+        path = Path(getInput())
+        if path.is_dir():
+            break
+        else:
+            showError(f'Path "{path.resolve()}" is not a directory.')
+    
+    print(TextTheme.PROMPT + "\nDirectory tree: \n")
+    fullTree = ""
+    printTree(0, path)
+    showInfo("Directory scaned.")
+    
+    while True:
+        print(TextTheme.INPUT + "\nWould you like to copy this directory tree to the clipboard? (y/n)?")
+        _input = getInput()
+        if len(_input) == 1 and _input.lower() in ["y", "n"]:
+            if _input.lower() == "y":
+                try:
+                    pc.copy(fullTree)
+                    showInfo("Copied.")
+                except:
+                    showWarning("Copy failed. Your device may not support clipboard access. Please copy it manually.")
+            break
+    
+    input(TextTheme.PROMPT + "\nPress [Enter] to exit.")
+    sys.exit()
+    
 if __name__ == "__main__":
     main()
